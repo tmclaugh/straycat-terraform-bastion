c3482a8d83acee8c5aebcbed47d8f3b4bb91904e
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2017-03-07 16:13:32 -0500

    Switch from naming our service to just calling it svc.
    Makes copying this pattern easier.  If we need to use these resources we'll be
    naming the data soruce anyways.

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ resource "github_repository" "bastion" {
 /*
 * Deploy Bastion host.
 */
-module "bastion" {
+module "svc" {
   source                = "github.com/tmclaugh/tf_straycat_svc"
   svc_name              = "${var.svc_name}"
   aws_account           = "${var.aws_account}"
@@ -68,12 +68,12 @@ module "bastion" {
   security_group_service_ingress_external = "${var.security_group_service_ingress}"
 }
 
-resource "aws_security_group_rule" "bastion_ssh_ingress" {
+resource "aws_security_group_rule" "svc_ssh_ingress" {
   type                      = "ingress"
   from_port                 = "${var.security_group_default_ingress["from_port"]}"
   to_port                   = "${var.security_group_default_ingress["to_port"]}"
   protocol                  = "${var.security_group_default_ingress["protocol"]}"
-  source_security_group_id  = "${module.bastion.security_group_id}"
+  source_security_group_id  = "${module.svc.security_group_id}"
   security_group_id         = "${data.terraform_remote_state.aws_vpc.vpc.default_security_group_ids[var.security_group_access]}"
 }
 
@@ -81,70 +81,70 @@ resource "aws_security_group_rule" "bastion_ssh_ingress" {
 * outputs
 */
 output "launch_config_id" {
-  value = "${module.bastion.launch_config_id}"
+  value = "${module.svc.launch_config_id}"
 }
 
 output "autoscaling_group_id" {
-  value = "${module.bastion.autoscaling_group_id}"
+  value = "${module.svc.autoscaling_group_id}"
 }
 
 output "autoscaling_group_name" {
-  value = "${module.bastion.autoscaling_group_name}"
+  value = "${module.svc.autoscaling_group_name}"
 }
 
 output "autoscaling_group_availability_zones" {
-  value = "${module.bastion.autoscaling_group_availability_zones}"
+  value = "${module.svc.autoscaling_group_availability_zones}"
 }
 
 output "autoscaling_group_min_size" {
-  value = "${module.bastion.autoscaling_group_min_size}"
+  value = "${module.svc.autoscaling_group_min_size}"
 }
 
 output "autoscaling_group_max_size" {
-  value = "${module.bastion.autoscaling_group_max_size}"
+  value = "${module.svc.autoscaling_group_max_size}"
 }
 
 output "autoscaling_group_desired_capacity" {
-  value = "${module.bastion.autoscaling_group_desired_capacity}"
+  value = "${module.svc.autoscaling_group_desired_capacity}"
 }
 
 output "autoscaling_group_launch_configuration" {
-  value = "${module.bastion.autoscaling_group_launch_configuration}"
+  value = "${module.svc.autoscaling_group_launch_configuration}"
 }
 
 output "autoscaling_group_vpc_zone_identifier" {
-  value = "${module.bastion.autoscaling_group_vpc_zone_identifier}"
+  value = "${module.svc.autoscaling_group_vpc_zone_identifier}"
 }
 
 output "iam_role_arn" {
-  value = "${module.bastion.iam_role_arn}"
+  value = "${module.svc.iam_role_arn}"
 }
 
 output "iam_role_name" {
-  value = "${module.bastion.iam_role_name}"
+  value = "${module.svc.iam_role_name}"
 }
 
 output "iam_instance_profile_arn" {
-  value = "${module.bastion.iam_instance_profile_arn}"
+  value = "${module.svc.iam_instance_profile_arn}"
 }
 
 output "iam_instance_profile_name" {
-  value = "${module.bastion.iam_instance_profile_name}"
+  value = "${module.svc.iam_instance_profile_name}"
 }
 
 output "iam_instance_profile_roles" {
-  value = "${module.bastion.iam_instance_profile_roles}"
+  value = "${module.svc.iam_instance_profile_roles}"
 }
 
 output "security_group_id" {
-  value = "${module.bastion.security_group_id}"
+  value = "${module.svc.security_group_id}"
 }
 
 output "security_group_vpc_id" {
-  value = "${module.bastion.security_group_vpc_id}"
+  value = "${module.svc.security_group_vpc_id}"
 }
 
 output "security_group_name" {
-  value = "${module.bastion.security_group_name}"
+  value = "${module.svc.security_group_name}"
 }
 
